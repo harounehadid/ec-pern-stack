688fb033a21ca17d276f7d7aa9f6f7993d39b080
Haroune Hadid <harounehadidbusiness@gmail.com>
2023-07-24 12:42:07 +0100

    Create filter item

--- client/src/components/filter-item/FilterItem.js
+++ client/src/components/filter-item/FilterItem.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from './filterItem.module.scss';
+import { icons } from '../../media/mediaHolder';
 
 const FilterItem = (props) => {
   const { name } = props;
   
   return (
-    <div>{name}</div>
+    <a className={styles['container']}>
+      <span>{name}</span>
+      <img className={styles['icon']} src={icons.dropdown} alt='' />
+    </a>
   )
 }
 
