a334ea65887446ffa3bc665c37439db96b0d08fc
Haroune Hadid <harounehadidbusiness@gmail.com>
2023-07-24 14:04:57 +0100

    Add product showcase component

---
+++ client/src/components/product-showcase/ProductShowcase.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import styles from './productShowcase.module.scss';
+import ProductCard from '../product-card/ProductCard';
+
+const ProductShowcase = () => {
+    const products = [
+        {
+            name: 'product1',
+            price: '2500 DZD'
+        },
+        {
+            name: 'product2',
+            price: '3700 DZD'
+        },
+        {
+            name: 'product3',
+            price: '5500 DZD'
+        },
+        {
+            name: 'product4',
+            price: '15500 DZD'
+        },
+        {
+            name: 'product5',
+            price: '7500 DZD'
+        },
+        {
+            name: 'product6',
+            price: '2400 DZD'
+        },
+        {
+            name: 'product7',
+            price: '1000 DZD'
+        },
+        {
+            name: 'product8',
+            price: '10500 DZD'
+        },
+        {
+            name: 'product9',
+            price: '3400 DZD'
+        }
+    ];
+
+    return (
+        <div className={styles['container']}>
+            {
+                products.map(product => <ProductCard />)
+            }
+        </div>
+    )
+}
+
+export default ProductShowcase;
--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/header/Header';
 import Filter from '../components/filter/Filter';
+import ProductShowcase from '../components/product-showcase/ProductShowcase';
 
 export default function Home() {
   return (
     <>
         <Header />
         <Filter />
+        <ProductShowcase />
     </>
   )
 }
