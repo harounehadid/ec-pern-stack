f1a1881feea7ab2dd67f28024cd5a6e52186199f
Haroune Hadid <harounehadidbusiness@gmail.com>
2023-07-24 10:35:18 +0100

    Add media and media controller

---
+++ client/src/media/mediaHolder.js
@@ -0,0 +1,5 @@
+export const icons = {
+    searchbar: require('./icons/searchbar-icon.svg').default,
+    dropdown: require('./icons/dropdown-icon.svg').default,
+    cancel: require('./icons/cancel-icon.svg').default
+}
