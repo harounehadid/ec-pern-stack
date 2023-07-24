243d50a3a56f162814a174804a787ab11b5f9953
Haroune Hadid <harounehadidbusiness@gmail.com>
2023-07-24 11:12:30 +0100

    Add functionality to searchbar. Clean the component

--- client/src/components/searchbar/Searchbar.js
+++ client/src/components/searchbar/Searchbar.js
@@ -1,31 +1,34 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { icons } from '../../media/mediaHolder';
 import styles from './searchbar.module.scss';
 
 const Searchbar = (props) => {
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-
-        setSearchTerm(e.target.value);
-    }
+    const {
+        handleChange, 
+        handleCancel, 
+        handleSubmit,
+        searchTerm
+    } = props;
 
     return (
-        <div className={styles['container']}>
-            <img className={styles['icon']} src={icons.searchbar} alt='' />
+        <form className={styles['container']} onSubmit={handleSubmit}>
+            <a onClick={handleSubmit}>
+                <img className={styles['icon']} src={icons.searchbar} alt='' />
+            </a>
 
             <input className={styles['searchbar-input']} 
                    type='search' 
                    placeholder='Search for products' 
                    id='searchbar' 
-                   name='searchbar' 
+                   name='searchbar'
+                   value={searchTerm}
+                   onChange={handleChange}
             />
 
-            <a className={styles['cancel-btn']}>
+            <a className={styles['cancel-btn']} onClick={handleCancel}>
                 <img src={icons.cancel} alt='' />
             </a>
-        </div>
+        </form>
     )
 }
 
--- client/src/components/searchbar/SearchbarContainer.js
+++ client/src/components/searchbar/SearchbarContainer.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Searchbar from './Searchbar';
 
 const SearchbarContainer = () => {
-  return <Searchbar />;
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e) => {
+      e.preventDefault();
+      setSearchTerm(e.target.value);
+  }
+
+  const handleCancel = (e) => {
+      e.preventDefault();
+      setSearchTerm('');
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(searchTerm);
+  }
+
+  return <Searchbar handleChange={handleChange}
+                    handleCancel={handleCancel}
+                    handleSubmit={handleSubmit}
+                    searchTerm={searchTerm} />;
 }
 
 export default SearchbarContainer;
