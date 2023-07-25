30aa9a4599a205a1e85942393ab863c23c88d82a
Haroune Hadid <harounehadidbusiness@gmail.com>
2023-07-25 12:23:41 +0100

    Connect the server to the database. Get the needed data.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./db');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/api/v1/products', (req, res) => {
+app.use(express.json());
+
+app.get('/api/v1/products', async (req, res) => {
+    const { rows } = await db.query('SELECT * FROM products');
+
     res.status(200).json({
         status: 'success',
         data: {
-            products: []
+            products: rows
         }
     })
 });
