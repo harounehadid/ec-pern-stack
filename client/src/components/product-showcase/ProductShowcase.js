e6f698b162c34cd7485acb68e89c2d4a076bbca6
Haroune Hadid <harounehadidbusiness@gmail.com>
2023-07-25 13:24:15 +0100

    Connect frontend to backend

--- client/src/components/product-showcase/ProductShowcase.js
+++ client/src/components/product-showcase/ProductShowcase.js
@@ -1,51 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './productShowcase.module.scss';
 import ProductCard from '../product-card/ProductCard';
+import productFinder from '../../api/productFinder';
 
 const ProductShowcase = () => {
-    const products = [
-        {
-            name: 'product1',
-            price: '2500 DZD'
-        },
-        {
-            name: 'product2',
-            price: '3700 DZD'
-        },
-        {
-            name: 'product3',
-            price: '5500 DZD'
-        },
-        {
-            name: 'product4',
-            price: '15500 DZD'
-        },
-        {
-            name: 'product5',
-            price: '7500 DZD'
-        },
-        {
-            name: 'product6',
-            price: '2400 DZD'
-        },
-        {
-            name: 'product7',
-            price: '1000 DZD'
-        },
-        {
-            name: 'product8',
-            price: '10500 DZD'
-        },
-        {
-            name: 'product9',
-            price: '3400 DZD'
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await productFinder.get('/');
+                console.log(response.data.data.products);
+                setProducts(response.data.data.products);
+            } catch (err) {
+                console.log(err);
+            }
         }
-    ];
+
+        fetchData();
+
+    }, []);
 
     return (
         <div className={styles['container']}>
             {
-                products.map(product => <ProductCard name={product.name} price={product.price} />)
+                products.map(product => <ProductCard key={product.id} name={product.name} price={product.price} />)
             }
         </div>
     )
--- server/server.js
+++ server/server.js
@@ -1,22 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const db = require('./db');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/api/v1/products', async (req, res) => {
-    const { rows } = await db.query('SELECT * FROM products');
+    try {
+        const { rows } = await db.query('SELECT * FROM products');
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            products: rows
-        }
-    })
+        res.status(200).json({
+            status: 'success',
+            results: rows.length,
+            data: {
+                products: rows
+            }
+        });
+    } catch(err) {
+        console.log(err);   
+    }
 });
 
 app.listen(PORT, () => {
